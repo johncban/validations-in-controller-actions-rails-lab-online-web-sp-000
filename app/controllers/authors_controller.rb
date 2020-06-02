387b7219b1a150fde8db5d9dd45ca5b21befc479
Juan Carlo Banayo <johncban@gmail.com>
2020-06-02 09:50:03 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,13 +8,12 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.new(author_params)
-
-    if @author.save
-      redirect_to author_path(@author)
-    else
-      render :new
-    end
+    @author = Author.create!(author_params)
+      if @author.save
+        redirect_to author_path(@author)
+      else
+        render :new
+      end
   end
 
   private
